3c733d1ac6fc8b17d32ed9f46c69438a004c1834
kaliganoff <kaliganoff@yahoo.com>
2024-09-12 00:46:47 +0300

    feat: Add pagination and the modal for new ads

--- src/pages/AllAdsPage/AllAdsPage.tsx
+++ src/pages/AllAdsPage/AllAdsPage.tsx
@@ -2,10 +2,30 @@ import { useEffect, useState } from "react";
 import getAllAds from "../../services/getAllAds";
 import { Advertisment } from "../../types/types";
 import AdCard from "../../components/AdCard/AdCard";
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Select, Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton, 
+  useDisclosure,
+  Input,
+  Text} from "@chakra-ui/react";
+import getAdsPaginated from "../../services/getAdsPaginated";
+import createAd from "../../services/createAd";
 
 export default function AllAdsPage() {
   const [ads, setAds] = useState<Advertisment[]>([]);
+  const [adsOnPage, setAdsOnPage] = useState<Advertisment[]>([]);
+  const [itemsPerPage, setItemsPerPage] = useState<number>(10);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [pictureModal, setPictureModal] = useState('');
+  const [nameModal, setNameModal] = useState('');
+  const [descriptionModal, setDescriptionModal] = useState('');
+  const [priceModal, setPriceModal] = useState('');
+  const [needUpdate, setNeedUpdate] = useState(false);
 
   useEffect(() => {
     async function setAllAds() {
@@ -13,12 +33,85 @@ export default function AllAdsPage() {
       setAds(allAds);
     }
     setAllAds();
-  }, []);
+  }, [needUpdate]);
+
+  useEffect(() => {
+    async function setAdsPaginated() {
+      const adsPaginated = await getAdsPaginated(currentPage, itemsPerPage);
+      setAdsOnPage(adsPaginated);
+    }
+    setAdsPaginated();
+    }, [itemsPerPage, currentPage, needUpdate]);
+
+    
+  const renderPagination = () => {
+    const pages = [];
+    const totalPages = Math.ceil(ads.length / itemsPerPage);
+    for (let i = 1; i <= totalPages; i += 1) {
+      pages.push(
+        <Button
+          key={i}
+          onClick={() => setCurrentPage(i)}
+          isDisabled={i === currentPage}
+          mx={1}
+          size="sm"
+          bgColor={i === currentPage ? "#ded6cb" : "gray.200"}
+        >
+          {i}
+        </Button>,
+      );
+    }
+    return pages;
+  };
+
+  function HandleCreateAd() {
+    createAd(pictureModal, nameModal, descriptionModal, priceModal);
+    setPictureModal('');
+    setNameModal('');
+    setDescriptionModal('');
+    setPriceModal('');
+    setNeedUpdate(prev => !prev);
+  }
+
   return (
     <Box>
-      {ads.map((ad) => (
+      {adsOnPage.map((ad) => (
         <AdCard ad={ad} />
       ))}
+      {renderPagination()}
+      <Select placeholder="Количество объявлений на странице" onChange={(e) => setItemsPerPage(+e.target.value)}>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </Select>
+      <Button onClick={onOpen}>Создать новое объявление</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Создать новое объявление</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Картинка:</Text>
+            <Input value={pictureModal} onChange={(e) => setPictureModal(e.target.value)}></Input>
+            <Text>Название:</Text>
+            <Input value={nameModal} onChange={(e) => setNameModal(e.target.value)}></Input>
+            <Text>Описание:</Text>
+            <Input value={descriptionModal} onChange={(e) => setDescriptionModal(e.target.value)}></Input>
+            <Text>Стоимость:</Text>
+            <Input value={priceModal} onChange={(e) => setPriceModal(e.target.value)}></Input>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button onClick={() => HandleCreateAd()} variant='ghost'>Создать объявление</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
---
+++ src/services/createAd.ts
@@ -0,0 +1,19 @@
+import { endpoint } from "../consts/consts";
+
+export default async function createAd(imageUrl: string, name: string, description: string, price: string) {
+    fetch(`${endpoint}/advertisements`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+            "name": name,
+            "price": price,
+            "createdAt": new Date().toISOString(),
+            "views": 0,
+            "likes": 0,
+            "imageUrl": imageUrl,
+            "description": description
+          })
+      });
+}
